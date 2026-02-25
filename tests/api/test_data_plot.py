503867c7617bf3dcdf5717f38c4af582d7aa85e7
tekefene <icseig@outlook.hu>
2026-02-25 21:06:03 +0100

    Update test_data_plot.py

--- tests/api/test_data_plot.py
+++ tests/api/test_data_plot.py
@@ -5,8 +5,8 @@ import pandas as pd
 import pytest
 from fastapi.testclient import TestClient
 
-from app.main import app
 from app.core.auth import get_current_user_id
+from app.main import app
 
 
 @pytest.fixture(autouse=True)
