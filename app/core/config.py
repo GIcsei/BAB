da9c882f02d71538c34af344a8725440c7e60a78
tekefene <icseig@outlook.hu>
2026-03-01 15:24:04 +0100

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -33,30 +33,20 @@ def _to_int(value: Optional[str], default: int) -> int:
 class Settings:
     raw_app_user_data_dir: Optional[str]
     app_user_data_dir: Path
-    netbank_base_dir: Path  # ?
+    netbank_base_dir: Path # TODO: Should be used to security store netbank credentials and keys instead of app_user_data_dir
     allow_unsafe_deserialize: bool  # TODO: Remove this flag and related code once safe deserialization is implemented
     app_job_hour: int
     app_job_minute: int
     google_application_credentials: Optional[
         Path
-    ]  # Same as netbank_master_key, should we merge them?
-    firebase_project_id: Optional[
-        str
-    ]  # Coming from json file, should we merge it with google_application_credentials?
-    firebase_test_project_id: Optional[
-        str
-    ]  # Only used for testing, should we merge it with firebase_project_id and google_application_credentials?
-    firebase_api_key: Optional[
-        str
-    ]  # API key for Firebase, should we merge it with google_application_credentials?
+    ]
     log_level: str
     log_file: str
     log_json: bool
     selenium_downloads_dir: Optional[
         str
-    ]  # Not used in the codebase, should we remove it or implement it?
+    ]
     local_downloads_dir: Optional[str]
-    netbank_master_key: Optional[str]
     is_testing: bool  # Based on flag, default test values shall be loaded. After implementation, other test params can be removed and set based on this flag.
 
     def __post_init__(self):
@@ -113,18 +103,12 @@ def get_settings() -> Settings:
     )
 
     logger.info("Loading application credentials...")
-    google_application_credentials_env = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    google_application_credentials_env = os.getenv("FIRESTORE_CREDENTIALS")
     google_application_credentials = (
         Path(google_application_credentials_env)
         if google_application_credentials_env
         else None
     )
-
-    firebase_project_id = os.getenv("FIREBASE_PROJECT_ID")
-    firebase_test_project_id = os.getenv("FIREBASE_TEST_PROJECT_ID")
-    firebase_api_key = os.getenv("FIREBASE_API_KEY")
-
-    netbank_master_key = os.getenv("NETBANK_MASTER_KEY")
 
     logger.info("Loading logging settings...")
 
@@ -155,15 +139,11 @@ def get_settings() -> Settings:
         app_job_hour=app_job_hour,
         app_job_minute=app_job_minute,
         google_application_credentials=google_application_credentials,
-        firebase_project_id=firebase_project_id,
-        firebase_test_project_id=firebase_test_project_id,
-        firebase_api_key=firebase_api_key,
         log_level=log_level,
         log_file=log_file,
         log_json=log_json,
         selenium_downloads_dir=selenium_downloads_dir,
         local_downloads_dir=local_downloads_dir,
-        netbank_master_key=netbank_master_key,
         is_testing=is_testing,
     )
     return _SETTINGS
